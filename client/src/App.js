5f0211cfb29b8f263f7d739e5e6112bf1428c0b4
samsonmeseret <samsonmeseretb@gmail.com>
2022-06-25 14:08:57 -0400

    client side

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import MaterialTable from "material-table";
+import tableIcons from "./Icons";
+
+class App extends Component {
+  render() {
+    return (
+      <div style={{ maxWidth: "100%" }}>
+        <MaterialTable
+          columns={[
+            { title: "Adı", field: "name" },
+            { title: "Soyadı", field: "surname" },
+            { title: "Doğum Yılı", field: "birthYear", type: "numeric" },
+            {
+              title: "Doğum Yeri",
+              field: "birthCity",
+              lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
+            },
+          ]}
+          data={[
+            {
+              name: "Mehmet",
+              surname: "Baran",
+              birthYear: 1987,
+              birthCity: 63,
+            },
+          ]}
+          title="Demo Title"
+          icons={tableIcons}
+          options={{
+            exportButton: true
+          }}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
