2b2ef1486c52102c8f7117013737c40cec75a788
samsonmeseret <samsonmeseretb@gmail.com>
2022-07-07 17:41:11 -0400

    hero updated

--- client/src/components/Hero/Hero.js
+++ client/src/components/Hero/Hero.js
@@ -20,15 +20,7 @@ const Hero = () => {
           </a>
         </div>
         <div className="hero-image">
-          <SvgBlob
-            variant="pattern"
-            pattern={{ allPatterns: 2 }}
-            color="#d1d8e0"
-            shapeProps={{ size: 300, growth: 20, edges: 9 }}
-            isOutline={0.3 < 0.5}
-          >
-            <Kume p="width:340 height:480" />
-          </SvgBlob>
+          <Kume p="width:340 height:480" />
         </div>
       </section>
     </>
