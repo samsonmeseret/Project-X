21da51bde55b89c00a0a4fbd39a6df7d076b360e
samsonmeseret <samsonmeseretb@gmail.com>
2022-07-13 11:50:22 -0400

    footer updated

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -8,10 +8,22 @@ export default function Footer() {
   return (
     <>
       <div className="footer">
-        <div>
-          <FacebookIcon />
-          <TelegramIcon />
-          <LinkedInIcon />
+        <footer>
+          <div className="footer-social">
+            <p>Follow as on</p>
+            <div>
+              <FacebookIcon />
+              <TelegramIcon />
+              <LinkedInIcon />
+            </div>
+          </div>
+          <div className="footer-links">
+            <ul></ul>
+            <ul></ul>
+          </div>
+        </footer>
+        <div className="copyright">
+          <p>Copyright `${new Date.getFullYear()}`, HealEye Clinic</p>
         </div>
       </div>
     </>
